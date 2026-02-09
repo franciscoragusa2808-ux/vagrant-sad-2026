a7122a6c9fbdf3b9987f1f941fee749f15d9059f
franciscoragusa <franciscoragusa2808@gmail.com>
2026-02-09 22:37:27 +0100

    se sube

--- proxy/provision.sh
+++ proxy/provision.sh
@@ -1,3 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+export DEBIAN_FRONTEND=noninteractive
+
+echo "########################################"
+echo " Aprovisionando proxy"
+echo "########################################"
+
+echo "Actualizando repositorios"
+apt-get update -y
+
+echo "Instalando squid"
+apt-get install -y squid
+
+echo "Creando estructura de directorios"
+mkdir -p /etc/squid/conf.d
+
 echo "Limpiando configuracion anterior"
 rm -rf /etc/squid/conf.d/*
 rm -f /etc/squid/dominios-denegados
@@ -5,15 +23,14 @@ rm -f /etc/squid/block-exp
 
 echo "Copiando configuracion desde el proyecto"
 
-# Configuración principal
 cp /vagrant/proxy/conf/squid.conf /etc/squid/
-
-# Todos los .conf (lan.conf, debian.conf, dmz.conf…)
 cp /vagrant/proxy/conf/*.conf /etc/squid/conf.d/
-
-# Ficheros auxiliares
 cp /vagrant/proxy/dominios-denegados /etc/squid/
 cp /vagrant/proxy/block-exp /etc/squid/
 
 echo "Reiniciando squid"
+systemctl enable squid
 systemctl restart squid
+
+echo "------ FIN PROXY ------"
+su
